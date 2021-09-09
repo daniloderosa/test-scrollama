1c077539f164e8fe423075722ed577870c404e82
Danilo De Rosa <74054364+daniloderosa@users.noreply.github.com>
2021-09-09 20:20:33 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+<script src="https://unpkg.com/intersection-observer"></script>
+<script src="https://unpkg.com/scrollama"></script>
+
+const scroller = scrollama();
+
+		scroller
+			.setup({
+				step: ".step",
+				progress: true
+			})
+			.onStepEnter((response) => {
+				console.log(response);
+
+				document.getElementById(response.element.dataset.target).setAttribute(response.element.dataset.attribute, response.element.dataset.value);
+			})
+			.onStepExit((response) => {
+				reset(response);
+			})
+			.onStepProgress((response) => {
+				if(response.element.dataset.type == 'progressive'){
+					console.log(response);
+					document.getElementById(response.element.dataset.target).setAttribute(response.element.dataset.attribute, response.progress * 100);
+				}
+			});
+
+		window.addEventListener("resize", scroller.resize);
+
+		function reset(response) {
+			if(response.element.dataset.attribute == 'r' && response.direction == 'up'){
+				document.getElementById('circObj').setAttribute('r', 100);
+			}
+			if(response.element.dataset.type == 'progressive' && response.direction == 'up'){
+				document.getElementById('circObj').setAttribute('r', 50);
+			}
+			if(response.element.dataset.attribute == 'opacity' && response.direction == 'up'){
+				document.getElementById('circObj').setAttribute('opacity', 1);
+			}
+		}
